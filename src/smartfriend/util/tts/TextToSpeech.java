be3f1bb484abf953863660236aaa2904ed248af9
isuri1 <isuuom2011@gmail.com>
2014-10-28 12:09:59 +0530

    initial project version

---
+++ src/smartfriend/util/tts/TextToSpeech.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package smartfriend.util.tts;
+
+import java.io.File;
+
+/**
+ *
+ * @author user
+ */
+
+public class TextToSpeech {
+    
+    public boolean speak(){
+        
+        return true;
+    }
+    
+    public boolean speak(File file,int [][]location){
+        
+        return true;
+    }
+    
+    public boolean speak(String word){
+        
+        return true;
+    }
+    
+    
+}
